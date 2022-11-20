a0477a00df37eacb6c4f1589027b227e56fac9f2
Adele <adele.pluquet@free.fr>
2022-11-20 10:20:41 +0100

    [UPD] TextBar

--- frontend/src/textField.tsx
+++ frontend/src/textField.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import useState from "react";
+import "./App.css";
 
-const TextField = () => {
-  return <input />;
-};
+
+const TextField = () => (
+  <form action="/" method="get">
+      <label htmlFor="header-search">
+          <span className="visually-hidden">Chargement d'un sample vers Samply</span>
+      </label>
+      <input
+          type="text"
+          id="header-search"
+          placeholder="URL to sample"
+          name="s" 
+      />
+      <button type="submit">Chargement</button>
+  </form>
+);
 
 export default TextField;
